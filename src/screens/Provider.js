9a81fca2af8e1db42c8300fe7485ee7726d76ca5
ARTHUR ALVES CALDEIRA <0057842@IFMG.EDU.BR>
2022-10-04 08:07:22 -0300

    Adicionando botão cadastrar em provider

--- src/screens/Provider.js
+++ src/screens/Provider.js
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import colors from '../global/colors';
 import Header from '../components/Header.js';
 import InputCadastro from '../components/InputCadastro.js';
+// import Footer from '../components/Footer.js';
 
 export default function Provider() {
     return (
@@ -22,8 +23,8 @@ export default function Provider() {
                     <InputCadastro placeholder="ENDEREÇO" icon='endereco' />
                 </View>
 
-                <TouchableOpacity>
-                    <Text style={styles.button}>Cadastrar</Text>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.textButton}>Cadastrar</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
@@ -44,6 +45,16 @@ const styles = StyleSheet.create({
         marginTop: 50,
     },
     button: {
-
+        backgroundColor: colors("verdeescuro"),
+        padding: 10,
+        width: '100%',
+        borderRadius: 12,
+        top: 15
+    },
+    textButton: {
+        fontSize: 20,
+        color: colors("branco"),
+        textTransform: 'uppercase',
+        textAlign: 'center'
     }
 });
