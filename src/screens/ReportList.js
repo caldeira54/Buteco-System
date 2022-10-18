27a508c873abdaacb7b7add3f617ca30da5d1242
Mateus Henrique <112947808+mateushen@users.noreply.github.com>
2022-10-18 08:07:44 -0300

    Update list

--- src/screens/ReportList.js
+++ src/screens/ReportList.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, StyleSheet} from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import colors from '../global/colors';
 import Card from '../components/Card';
@@ -8,52 +8,110 @@ import Footer from '../components/Footer';
 
 export default function ReportLists() {
 
-    const navigation = useNavigation();
+    const DATA = [
+        {
+            id: "1",
+            title: "Fornecedores",
+            type: "provider",
+        },
+        {
+            id: "2",
+            title: "Produtos",
+            type: "product",
+        },
+        {
+            id: "3",
+            title: "Vendas",
+            type: "sale",
+        },
+        {
+            id: "4",
+            title: "Compras",
+            type: "buy",
+        },
+        {
+            id: "5",
+            title: "Notas",
+            type: "note",
+        },
+    ];
 
-    const handlePressProviders = () => {
-        navigation.navigate("providers");
-    }
+    const Item = ({ item, onPress, type }) => (
+        <Card title={item.title} type={type} onPress={onPress} />
+    );
 
-    const handlePressSales = () => {
-        navigation.navigate("sales");
-    }
+    const [selectedId, setSelectedId] = useState(null);
 
-    const handlePressProducts = () => {
-        navigation.navigate("products");
-    }
+    const renderItem = ({ item }) => {
 
-    const handlePressBuys = () => {
-        navigation.navigate("buys");
-    }
-
-    const handlePressNotes = () => {
-        navigation.navigate("notes");
-    }
-
+        return (
+            <Item
+                item={item}
+                onPress={() => setSelectedId(item.id)}
+            />
+        );
+    };
 
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <Header title="RELATÓRIOS" backButton={false} />
-            <View style={styles.list}>
-                <Card title="Fornecedores" type="provider" onPress={handlePressProviders} />
-                <Card title="Produtos" type="product" onPress={handlePressProducts} />
-                <Card title="Vendas" type="sale" onPress={handlePressSales} />
-                <Card title="Compras" type="buy" onPress={handlePressBuys} />
-                <Card title="Notas" type="note" onPress={handlePressNotes} />
-            </View>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                extraData={selectedId}
+            />
             <Footer />
-        </View>
+        </SafeAreaView>
     );
+
+    // const navigation = useNavigation();
+
+    // const handlePressProviders = () => {
+    //     navigation.navigate("providers");
+    // }
+
+    // const handlePressSales = () => {
+    //     navigation.navigate("sales");
+    // }
+
+    // const handlePressProducts = () => {
+    //     navigation.navigate("products");
+    // }
+
+    // const handlePressBuys = () => {
+    //     navigation.navigate("buys");
+    // }
+
+    // const handlePressNotes = () => {
+    //     navigation.navigate("notes");
+    // }
+
+
+    // return (
+    //     <View style={styles.container}>
+    //         <Header title="RELATÓRIOS" backButton={false} />
+    //         <View style={styles.list}>
+    //             <Card title="Fornecedores" type="provider" onPress={handlePressProviders} />
+    //             <Card title="Produtos" type="product" onPress={handlePressProducts} />
+    //             <Card title="Vendas" type="sale" onPress={handlePressSales} />
+    //             <Card title="Compras" type="buy" onPress={handlePressBuys} />
+    //             <Card title="Notas" type="note" onPress={handlePressNotes} />
+    //         </View>
+    //         <Footer />
+    //     </View>
+    // );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        height: '88%',
         backgroundColor: colors("verdeclaro"),
-        alignItems: 'center'
+        alignItems: 'center',
     },
     list: {
+        display: 'flex',
         flex: 1,
-        alignItems: 'center',
-    }
+    },
+
 })
