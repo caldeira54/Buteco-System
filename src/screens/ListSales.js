e191400c1366cac9e6e3b5e8857ed790b8b6c7c5
ARTHUR ALVES CALDEIRA <0057842@ifmg.edu.br>
2022-10-18 08:29:31 -0300

    Criação de componentes para o botão de editar e excluir mais a criação da lista de vendas

--- src/screens/ListSales.js
+++ src/screens/ListSales.js
@@ -2,21 +2,41 @@ import React from 'react';
 import { View, StyleSheet, SafeAreaView } from 'react-native';
 import colors from '../global/colors';
 import Footer from '../components/Footer';
+import BtnEditar from '../components/BtnEditar';
+import BtnExcluir from '../components/BtnExcluir';
+import Header from '../components/Header';
 
 export default function ListSales() {
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.container}>
-                <Footer />
-            </View>
-        </SafeAreaView>
+        <>
+            <SafeAreaView style={styles.container}>
+                <Header title="RELATÓRIO DE VENDAS"/>
+                    <View style={styles.position}>
+                        <BtnEditar />
+                        <BtnExcluir />
+                    </View>
+            </SafeAreaView>
+            <Footer />
+        </>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        height: '88%',
         backgroundColor: colors("verdeclaro"),
         alignItems: 'center'
     },
+    position: {
+        display: 'flex',
+        flexDirection: 'row',
+        bottom: 0,
+        position: 'absolute',
+        justifyContent: 'space-evenly',
+        width: '75%',
+        marginBottom: 30
+    },
+    button: {
+        
+    }
 })
