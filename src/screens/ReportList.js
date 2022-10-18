bb5bff51635f0e674fc1fe14fd51ad0e0d617b0f
Mateus Henrique <112947808+mateushen@users.noreply.github.com>
2022-10-18 08:09:50 -0300

    Update ReportList.js

--- src/screens/ReportList.js
+++ src/screens/ReportList.js
@@ -68,15 +68,11 @@ export default function ReportLists() {
     );
 
     const renderItem = ({ item }) => {
-        const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-        const color = item.id === selectedId ? 'white' : 'black';
 
         return (
             <Item
                 item={item}
                 onPress={() => setSelectedId(item.id)}
-                backgroundColor={{ backgroundColor }}
-                textColor={{ color }}
             />
         );
     };
